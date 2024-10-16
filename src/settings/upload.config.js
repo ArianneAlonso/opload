e11ff34db929967e3bd16e77b0a37e9a28111b73
ArianneAlonso <ariannealonso8@gmail.com>
2024-10-16 16:48:33 -0300

    update settings

--- src/settings/upload.config.js
+++ src/settings/upload.config.js
@@ -3,7 +3,13 @@ import path from "node:path";
 import crypto from "node:crypto";
 
 //storage
-
+const storage = multer.diskStorage({
+    destination: (_req, _file, cb) =>{
+        cb(null, "src/uploads/");
+    },
+    filename: (_req, _file, cb)=>{
+        cb(null, crypto.randomUUID().toString()+ path.extname(file.originalname));
+    },});
 //limits
 
 //filters
